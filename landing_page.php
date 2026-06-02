0f30c58cf7cff3ecd932dd88eb14b0aa7f4909b8
DanaTada <150678785+DanaTada@users.noreply.github.com>
2026-06-03 02:13:54 +0300

    Add landing_page.php
    landing_page.php with 2 empty buttons (#1)

---
+++ landing_page.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>"DigiMath" sākumlapa</title>
+    </head>
+    <body>
+        <main>
+             <h1>"DigiMath" sākumlapa !</h1>
+        </main>
+    </body>
+</html>
