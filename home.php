ce4b94d31a09a97ca62b2c71e711390a5376a39b
alisekalinina <150857894+alisekalinina@users.noreply.github.com>
2026-06-04 22:18:32 +0300

    Add home.php with user profile and class selection

---
+++ home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+    <meta charset="UTF-8">
+    <title>Sākums</title>
+</head>
+<body>
+
+    <h1>Mans profils</h1>
+    <p>Lietotājvārds: </p>
+
+    <button type="button">Mans profils</button>
+    <button type="button">Mans progress</button>
+    <button type="button">Iziet</button>
+
+    <h2>Izvēlies klasi</h2>
+    <div>
+        <button type="button">1</button>
+        <button type="button">2</button>
+        <button type="button">3</button>
+        <button type="button">4</button>
+        <button type="button">5</button>
+        <button type="button">6</button>
+        <button type="button">7</button>
+        <button type="button">8</button>
+        <button type="button">9</button>
+    </div>
+
+</body>
+</html>
