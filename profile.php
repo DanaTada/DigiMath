23dd73d2046b499ab48ad9ec1481c5df8ef0076c
alisekalinina <150857894+alisekalinina@users.noreply.github.com>
2026-06-05 15:34:29 +0300

    Create profile.php for user profile display

---
+++ profile.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+    <meta charset="UTF-8">
+    <title>Mans profils</title>
+</head>
+<body>
+
+    <h1>Mans profils</h1>
+
+    <p>Vārds: </p>
+    <p>Lietotājvārds: </p>
+    <p>Klase: </p>
+
+    <button type="button" onclick="location.href='home.php'">Atpakaļ</button>
+
+</body>
+</html>
